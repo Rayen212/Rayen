ad156313d63ebe1e0d8b1dfffadfd2e481ed87fe
Rayen <Rayen@sys.leca39.cl>
2024-07-30 10:35:52 -0400

    primera version del proyecto

---
+++ src/service/productoService.js
@@ -0,0 +1,19 @@
+import config from '../config/knexConfig.js'
+import knex from 'knex'
+export default class ProductService{
+    constructor(){
+        this.knex = knex(config)
+    }
+
+    getProductos = async() =>{
+        const productos = await this.knex.from('productos').select('*')
+        return Object.values(JSON.parse(JSON.stringify(productos)))
+    }
+    addProductos = async(producto) =>{
+        const respuesta = this.knex("productos").insert(producto)
+        return respuesta
+    }
+    updateProductoById = async(id,producto) => {
+       return this.knex('productos').where({producto_id:id}).update(producto)
+}
+}
